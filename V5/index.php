ad4c78ec6445f29a2cb2ebbdb5218c26f57ff8dd
Domenico <ferraro.domenico125@gmail.com>
2017-05-08 18:47:14 +0200

    Aggiungo progetto Netbeans

---
+++ V5/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="card">
+            <img src="img_avatar.png" alt="Domenico" style="width:100%">
+            <div class="container">
+                <h4><b>Nome Cognome</b></h4> 
+                <p>Titolo tesina</p> 
+            </div>
+        </div>  
+    </body>
+</html>
