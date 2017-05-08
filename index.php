ae33dcdd19449c8c5a70b30ff9d58e43336d3bbe
dawidfts <dawid.fts@gmail.com>
2017-05-08 20:48:07 +0200

    index update 8/05
    creazione della base della pagina index con le foto e il testo di prova

--- index.php
+++ index.php
@@ -3,22 +3,32 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="indexstyle.css">
         <title>Index</title>
+        <link rel="stylesheet" href="indexstyle.css">
+        
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     </head>
     <body>
         <div id = "container">
-			<div id = "header"></div>
+			<div id = "header">VH</div>
 			
 			<div id = "slider"></div>
-			<div id = "data"></div>
+			<div id = "data">
+				08/Maggio/2017</br>
+				Lunedi</br>
+				20:21</br>
+			</div>
 			<div style = "clear: both;"></div>
 			
-			<div id = "dots"></div>
-			<div id = "button"></div>
+			<div id = "dots">
+				<p>OOOO</p>
+			</div>
+			<div id = "button">PULSANTE HOME</div>
 			<div style = "clear: both;"></div>
 			
-			<div id = "footer"></div>
+			<div id = "footer">
+				Realizzato da Dawid Jakub Grzelczyk and Domenico Ferraro &copy; Tutti diritti riservati
+			</div>
         </div>  
     </body>
 </html>
