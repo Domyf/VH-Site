627d23477b20e1b3dad1e9c1da2f88469d12f3cb
Domenico <ferraro.domenico125@gmail.com>
2017-05-08 19:39:29 +0200

    Test background home

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="homestyle.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="page-content">
+            <br><br><br>
+            <div class="card" style="margin-left: 200px;">
+                <!--<img src="img_avatar.png" alt="Domenico" style="width:100%">-->
+                <div class="card-content">
+                    <h4><b>Nome Cognome</b></h4> 
+                    <p>Titolo Tesina</p> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
