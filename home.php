942e707e91e7f20b348218afa2dc224cf7ddf3cc
dawidfts <dawid.fts@gmail.com>
2017-05-13 18:27:12 +0200

    update slider

--- home.php
+++ home.php
@@ -5,8 +5,8 @@
         <link rel="stylesheet" href="homestyle.css">
         <title>Home</title>
     </head>
-    <body>
-        <div style="background: black; position: fixed; width: 100%;">
+    <body onload="cambia_slide()">
+        <div style="background: black; width: 100%;">
         <div class="topnav horizontal-centered" id="homeTopNav">
             <a href="#home">Home</a>
             <a href="#news">Elemento 2</a>
@@ -15,6 +15,12 @@
             <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="showMenu()">&#9776;</a>
         </div>
         </div>
+        
+        <div id ="foto_auto" class = "horizontal-centered">
+        	<div id = "slider"></div>
+        	<div id = "desc"></div>
+        </div>
+        
         <div class="page-content horizontal-centered">
             <p>Contenuto pagina</p>
             <p>Contenuto pagina</p>
@@ -47,6 +53,17 @@
                     x.className = "topnav";
                 }
             }
+
+
+			var comodo = -1;
+			function cambia_slide()
+			{
+				comodo++;
+				if (comodo>2) comodo =0;
+				var file ="<img src=\"img/slide/slide"+comodo+".jpg\"/>";
+				document.getElementById("slider").innerHTML = file;
+				setTimeout("cambia_slide()", 5000)
+			}
         </script>
     </body>
 </html>
