8bdce5ce34637f26ca2d051bd9b63cae4f959a5b
Domenico <ferraro.domenico125@gmail.com>
2017-05-14 21:04:40 +0200

    QUERY A

--- home.php
+++ home.php
@@ -15,8 +15,8 @@
                 <a href="restituisci.php"><button type="button"class="btn btn-menu">Restituisci</button></a>
                 <a href="soci.php"><button type="button"class="btn btn-menu">Soci</button></a>
             <div style="float:right;">
-                <form action="home.php" method="POST" style="margin-right: 10px; font-size: 18px;">Dal<input type="date" name="start" min="2000-01-01" class="datepicker">
-                    Al<input type="date" name="end" min="2020-01-01" class="datepicker">
+                <form action="home.php" method="POST" style="margin-right: 10px; font-size: 18px;">Dal<input type="date" name="start" class="datepicker">
+                    Al<input type="date" name="end" class="datepicker">
                     <input type="submit" value="Aggiorna" class="btn btn-aggiorna">
                 </form>
             </div>
@@ -24,12 +24,14 @@
         </div>
         <div class="page-content horizontal-centered">
         <?php
-            include 'connect.php';if (isset($_POST["start"]) && isset($_POST["end"])) {
+            include 'connect.php';
+            $result;
+            if (isset($_POST["start"]) && isset($_POST["end"])) {
                 //QUERY 1
                 $inizio = $_POST["start"];
                 $fine = $_POST["end"];
-                
-                $sql = "SELECT * FROM Auto WHERE Targa IN (SELECT Auto FROM Noleggi WHERE );";
+                $sql = "SELECT * FROM Auto WHERE Targa NOT IN (SELECT Auto FROM Noleggi WHERE ('$inizio' >= Inizio && '$inizio' <= Fine) OR ('$fine' >= Inizio && '$fine' <= Fine));";
+                $result = mysqli_query($conn, $sql);
             } else {
                 //$sql = "SELECT * FROM Auto WHERE Targa IN (SELECT Auto FROM Noleggi WHERE AutoRestituita = 0);";
                 $sql = "SELECT * FROM Auto";
@@ -38,7 +40,8 @@
                 //Eseguo la query
                 //$result = $conn->query($sql);
                 $result = mysqli_query($conn, $sql);
-                if ($result->num_rows > 0) {
+            }
+            if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         echo '<div class="car-item">'
                         . '<img class="car-img" src="img/auto/'.$row["targa"].'.jpg">'
@@ -51,7 +54,6 @@
                         . '<button type="button"class="btn btn-noleggia car-item-btn">Noleggia</button></div></div>';
                     }
                 }
-            }
         ?>
             <div style="clear:both;"></div>
         </div>
