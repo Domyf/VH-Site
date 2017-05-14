e78d7f877696142e67f6bd2b0c6460c2a713d080
dawidfts <dawid.fts@gmail.com>
2017-05-14 20:40:41 +0200

    reindirizzamento su noleggia

--- noleggia.php
+++ noleggia.php
@@ -2,28 +2,26 @@
 <html lang="it">
     <head>
         <meta charset="utf-8" />
-        <title>Restituisci</title>
+        <title>Noleggia</title>
         <meta name="description" content="" />
         <meta name="keywords" content="" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <link rel="stylesheet" href="restituiscistyle.css" type="text/css">
         <link rel="stylesheet" href="homestyle.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     </head>
     <body>
         <div style = "position: relative;" class="car-list-menu" >
             <div class="horizontal-centered">
                 <a href="home.php"><button type="button"class="btn btn-menu">Home</button></a>
                 <a href="aggiungi.php"><button type="button"class="btn btn-menu">Aggiungi</button></a>
-                <a href="restituisci.php"><button type="button"class="btn btn-menu btn-selected">Restituisci</button></a>
+                <a href="restituisci.php"><button type="button"class="btn btn-menu">Restituisci</button></a>
                 <a href="soci.php"><button type="button"class="btn btn-menu">Soci</button></a>
             </div>
         </div>
         <div id="container">
-            <form action="" method="post">
-                <input type="text" name="Targa" placeholder="Targa">
-                <input type="text" name="CodFis" placeholder="Codice Fiscale">
-                <input type="submit" value="Restituisci" class="btn btn-restituisci">
-            </form>
+           <p style ="font-size: 24px;">Ops!</br>Questo sito è ancora in sviluppo.</br>Avrai la possibile di noleggiare la tua auto preferita
+           appena il sito web sarà online :)</p>
         </div>
         <div class="footer footer-registra">Realizzato da Dawid Grzelczyk e Domenico Ferraro</div>
     </body>
