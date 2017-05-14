ff62125703c7cabe52beda399829fe4462c06e0b
dawidfts <dawid.fts@gmail.com>
2017-05-14 21:35:49 +0200

    soci mezzo fatto

--- soci.php
+++ soci.php
@@ -19,17 +19,62 @@
             </div>
         </div>
         <div id="container">
-            <form action="" method="post">
-				<input type="text" name="c. fiscale" placeholder="c. fiscale">            	
-                <input type="text" name="Targa" placeholder="Targa">
+            <form action="soci.php" method="post">
+				<select name="codicefiscale" value="Codice Fiscale">
+                    <option disabled selected value>Scegli il codice fiscale</option>
+                <?php
+                include 'connect.php';
+
+                $sql = "SELECT CF FROM Soci";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        echo '<option value="'.$row["CF"].'">'.$row["CF"].'</option>';
+                    }
+                }
+                $conn->close();
+                ?>
+                </select>
+                
                 <div id = "data-input">
 	                Dal<input type="date" name="start" min="2000-01-01" class="datepicker"></br>
 	                Al<input type="date" name="end" min="2020-01-01" class="datepicker">
                 </div>
-                
+                                
                 <input type="submit" value="Cerca" class="btn btn-restituisci">
             </form>
-        </div>
+                </div>
+                <div>
+                <?php
+		            include 'connect.php';
+		            $result;
+		            if (isset($_POST["start"]) && isset($_POST["end"])) {
+		                //QUERY 2
+		                $socio = $_POST["codicefiscale"];
+		                $inizio = $_POST["start"];
+		                $fine = $_POST["end"];
+		                $sql = "SELECT * FROM Noleggi WHERE socio=$socio 
+								AND (SELECT Auto FROM Noleggi WHERE ('$inizio' <= Inizio && '$inizio' >= Fine)";
+		                $result = mysqli_query($conn, $sql);
+		            } else {
+		             	echo 'Nessuna auto nollegiata';
+		            }
+		            if ($result->num_rows > 0) {
+		                    while($row = $result->fetch_assoc()) {
+		                        echo '<div class="car-item">'
+		                        . '<img class="car-img" src="img/auto/'.$row["targa"].'.jpg">'
+		                        . '<div class="car-descr" style="text-align: center;">'
+		                        . '<div style="font-size: 12px; margin-top: 10px;">Marca</div>'
+		                        . '<div>'.$row["marca"].'</div>'
+		                        . '<div style="font-size: 12px; margin-top: 10px;">Modello</div>'
+		                        . '<div>'.$row["modello"].'</div>'
+		                        . '<div style="margin-top: 40px;">€'.$row["costo_giornaliero"].'/giorno.</div>'
+		                        . '<button type="button"class="btn btn-noleggia car-item-btn">Noleggia</button></div></div>';
+		                    }
+		                }
+        		?>
+        	</div>
         <div class="footer footer-registra">Realizzato da Dawid Grzelczyk e Domenico Ferraro</div>
     </body>
 </html>
